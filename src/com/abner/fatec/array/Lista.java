a68a03fc40a76672dfe7ae86a1449482055f9402
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-05-08 20:38:32 -0300

    style: tamanho lista

--- src/com/abner/fatec/array/Lista.java
+++ src/com/abner/fatec/array/Lista.java
@@ -6,15 +6,15 @@ public class Lista {
 
     public Lista(int capMax) {
         dados = new int[capMax];
-        tamanho = -1;
+        tamanho = 0;
     }
 
     public boolean vazia() {
-        return tamanho == -1;
+        return tamanho == 0;
     }
 
     public boolean cheia() {
-        return tamanho == dados.length - 1;
+        return tamanho == dados.length;
     }
 
     public void adicionaFinal(int e) throws Exception {
@@ -22,26 +22,26 @@ public class Lista {
             throw new Exception("A lista está cheia!"); // se a lista estiver cheia é disparado uma exceção que encerra o programa
         }
         // se o programa nao foi encerrado pela condição acima, então é executado as linhas abaixo
-        tamanho++;
         dados[tamanho] = e;
+        tamanho++;
     }
 
     public void adicionaInicio(int e) throws Exception {
         if (cheia()) {
             throw new Exception("A lista está cheia!");
         }
-        tamanho++;
         for (int i = tamanho; i > 0; i--) { // loop que pega elementos da esquerda e joga pra direita
             dados[i] = dados[i - 1];
         }
         dados[0] = e; // adicionando no inicio
+        tamanho++;
     }
 
     public int removeFinal() throws Exception {
         if (vazia()) {
             throw new Exception("A lista está vazia!");
         }
-        int r = dados[tamanho];
+        int r = dados[tamanho - 1];
         tamanho--;
         return r;
     }
@@ -51,7 +51,7 @@ public class Lista {
             throw new Exception("A lista está vazia!");
         }
         int r = dados[0];
-        for (int i = 0; i < tamanho; i++) { // vai percorrer a lista e sobrepor os elementos um indice pra tras
+        for (int i = 0; i < tamanho - 1; i++) { // vai percorrer a lista e sobrepor os elementos um indice pra tras
             dados[i] = dados[i + 1];
         }
         tamanho--;
@@ -64,7 +64,7 @@ public class Lista {
         }
         e--; // vai ser passado qual elemento quer, então diminui 1 pra pegar o indice certo
         int r = dados[e];
-        for (int i = e; i < tamanho; i++) {
+        for (int i = e; i < tamanho - 1; i++) {
             dados[i] = dados[i + 1];
         }
         tamanho--;
@@ -74,7 +74,7 @@ public class Lista {
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
-        for (int i = 0; i <= tamanho; i++) {
+        for (int i = 0; i < tamanho; i++) {
             s.append(dados[i]).append(" ");
         }
         return s.toString();
