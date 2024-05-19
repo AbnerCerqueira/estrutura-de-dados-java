2ea67c2e113a195112d77228f6f32d34d2b72c36
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-05-19 19:22:24 -0300

    feat: operacao adiciona final e remove final

--- src/com/abner/fatec/listaEncadeada/ListaEncadeada.java
+++ src/com/abner/fatec/listaEncadeada/ListaEncadeada.java
@@ -13,12 +13,42 @@ public class ListaEncadeada {
         inicio = novoNo;
     }
 
+    public void adicionaFinal(int e) {
+        if (vazia()) {
+            inicio = new No(e);
+            return;
+        }
+        No aux = inicio;
+        while (aux.prox != null) { // percorrer lista até o final
+            aux = aux.prox;
+        }
+        aux.prox = new No(e);
+    }
+
     public int removeInicio() throws Exception {
         if (vazia()) { // se tentar remover com a lista vazia o programa é encerrado
             throw new Exception("A lista está vazia");
         }
         int r = inicio.dado;
         inicio = inicio.prox;
+        return r;
+    }
+
+    public int removeFinal() throws Exception {
+        if (vazia()) {
+            throw new Exception("A lista está vazia!");
+        }
+        if (inicio.prox == null) { // caso tenha somente 1 nó na lista
+            int r = inicio.dado;
+            inicio = null;
+            return r;
+        }
+        No aux = inicio;
+        while (aux.prox.prox != null) { // percorrer até o penultimo nó
+            aux = aux.prox;
+        }
+        int r = aux.prox.dado; // pegar o ultimo (o aux parou no penultimo)
+        aux.prox = null; // garbage collector vai eliminar da memoria o nó que está no final
         return r;
     }
 
